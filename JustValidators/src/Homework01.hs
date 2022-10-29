5a28e1406dca5682d5aa387081577f53d18e6e54
Vortecsmaster <roberto.cerrud@vortecsla.com>
2022-10-29 11:08:46 -0500

    Session 2022-10-29 - Datum and Redeemer Validators

--- JustValidators/src/Homework01.hs
+++ JustValidators/src/Homework01.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds           #-}  --Enable datatype promotions
+{-# LANGUAGE DeriveGeneric       #-}
 {-# LANGUAGE FlexibleContexts    #-}  --Enable flexible contexts. Implied by ImplicitParams
 {-# LANGUAGE NoImplicitPrelude   #-}  --Don't load native prelude to avoid conflict with PlutusTx.Prelude
 {-# LANGUAGE ScopedTypeVariables #-}  --Enable lexical scoping of type variables explicit introduced with forall
@@ -21,7 +22,7 @@ import qualified Ledger.Scripts      as Scripts               -- Low Level Typed
 import           Ledger.Ada          as Ada
 --Plutus Off-Chain related - Contract Monad and Playground
 import           Plutus.Contract
-import           Playground.Contract (printJson, printSchemas, ensureKnownCurrencies, stage)
+import           Playground.Contract (printJson, printSchemas, ensureKnownCurrencies, stage, ToSchema)
 import           Playground.TH       (mkKnownCurrencies, mkSchemaDefinitions)
 import           Playground.Types    (KnownCurrency (..))
 import           Text.Printf         (printf)
@@ -31,6 +32,8 @@ import           Control.Monad       hiding (fmap)
 import           Data.Map            as Map
 import           Data.Text           (Text)
 import           Data.Void           (Void)
+import           Data.Aeson           (FromJSON, ToJSON)
+import           GHC.Generics         (Generic)          
 
 --ON-CHAIN RELATED CODE
 
@@ -44,43 +47,26 @@ validator :: Validator
 validator = mkValidatorScript $$(PlutusTx.compile [|| actualValidator ||])
 
 valHash :: Ledger.ValidatorHash
-valHash = Scripts.validatorHash validator  -- The hash of the validators
+valHash = Scripts.validatorHash validator  
 
 scrAddress :: Address
 scrAddress = scriptAddress validator 
 
 
 --THE OFFCHAIN RELATED CODE
+data GiveParams = GP { gpAmount :: Integer, gpDatum :: Integer } deriving (Generic, FromJSON, ToJSON, ToSchema)
+
 
 type GiftSchema =
-            Endpoint "give" Integer  --An Integer Parameter
+            Endpoint "give" GiveParams  
         .\/ Endpoint "grab" Integer
 
-give :: AsContractError e => Integer -> Contract w s e ()
-give amount = do
-    let tx = mustPayToOtherScript valHash (Datum $ Builtins.mkI 19) $ Ada.lovelaceValueOf amount      --This Tx needs an output, thats its going to be the Script Address, Datum MUST be specified, so is created and the ammount of lovelaces
+give :: AsContractError e => GiveParams -> Contract w s e ()
+give gparams = do
+    let tx = mustPayToOtherScript valHash (Datum $ Builtins.mkI $ gpDatum gparams) $ Ada.lovelaceValueOf $ gpAmount gparams      --This Tx needs an output, thats its going to be the Script Address, Datum MUST be specified, so is created and the ammount of lovelaces
     ledgerTx <- submitTx tx                                                                          --This line submit the Tx
     void $ awaitTxConfirmed $ getCardanoTxId ledgerTx                                                --This line waits for confirmation
-    logInfo @String $ printf "made a gift of %d lovelace" amount                                     --This line log info,usable on the PP(Plutus Playground)
---CLI VERSION
---alonzo-era \
--- $TESTNET \
-  --tx-in 4a5d4091600a8ea3eac1c3cde32d99c4514541888b071007a69ccdcf6dfab639#1 \
-  --tx-out $(cat validator.addr)+5100000 \
-  --tx-out-datum-hash-file 19datum.json \
-  --change-address $nami \
-  --out-file tx.unsigned
-
---cardano-cli transaction sign  \
-  --tx-body-file tx.unsigned  \
-  --signing-key-file Adr07.skey  \
-  -- $TESTNET  \
-  --out-file tx.signed
-
--- cardano-cli transaction submit  \
-  -- $TESTNET \
-  --tx-file tx.signed
-
+    logInfo @String $ printf "made a gift of %d lovelace" $ gpAmount gparams                                     --This line log info,usable on the PP(Plutus Playground)
 
 
 grab :: forall w s e. AsContractError e => Integer -> Contract w s e ()                                     
@@ -95,33 +81,6 @@ grab n = do
     ledgerTx <- submitTxConstraintsWith @Void lookups tx                                             -- Allow the wallet to construct the tx with the necesary information
     void $ awaitTxConfirmed $ getCardanoTxId ledgerTx                                                -- Wait for confirmation
     logInfo @String $ "collected gifts"                                                              -- Log information 
-
-{-
-cardano-cli transaction build \
-  --alonzo-era \
-  --testnet-magic 1097911063 \
-  --tx-in fa3af564fff04bd5b77a9a39f0e6055acf7eeb7358516754855b30fcbb4524d5#1 \
-  --tx-in-script-file ./vesting.plutus \
-  --tx-in-datum-file ./unit.json  \
-  --tx-in-redeemer-file ./unit.json \
-  --required-signer-hash 1dcdf420c1488ba345730d41f72a846428ba814c2bd639462eaf5a07 \
-  --tx-in-collateral 67557dbe63d46e252276c729ebf75afe615c9903b644a37c11f6f0ac22fa8aff#0 \
-  --change-address $nami \
-  --invalid-before 61967066 \
-  --protocol-params-file ./protocol.params \
-  --out-file tx.body
-
-cardano-cli transaction sign \
-    --tx-body-file tx.body \
-    --signing-key-file payment2.skey \
-    --testnet-magic 1097911063 \
-    --out-file tx.signed
-
-cardano-cli transaction submit \
-    --testnet-magic 1097911063 \
-    --tx-file tx.signed
--}
-
 
 endpoints :: Contract () GiftSchema Text ()
 endpoints = awaitPromise (give' `select` grab') >> endpoints                                         -- Asynchronously wait for the endpoints interactions from the wallet
